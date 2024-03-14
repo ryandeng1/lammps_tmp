e5b346911ad90173605b5cc37fc07ea4bea5d5aa
ryandeng1 <ryandeng@mit.edu>
2024-03-13 21:04:26 -0400

    re-enable irecv in stencil md

--- src/verlet.cpp
+++ src/verlet.cpp
@@ -60,7 +60,7 @@ using namespace LAMMPS_NS;
 
 static constexpr bool TEST_AGAINST_LAMMPS_LOCAL = TEST_AGAINST_LAMMPS;
 static constexpr bool ENABLE_SEND_THREADS = false;
-static constexpr bool ENABLE_RECV_THREADS = false;
+static constexpr bool ENABLE_RECV_THREADS = true;
 
 /* ---------------------------------------------------------------------- */
 
