f5ec2a0670cc1e580fe37e66fd42df9b992dbe31
ryandeng1 <ryandeng@mit.edu>
2024-03-22 13:37:15 -0400

    change to ifdef lmp_openmp

--- src/lammps.h
+++ src/lammps.h
@@ -65,7 +65,7 @@ class LAMMPS {
   std::vector<std::array<class Neighbor*, NUM_TIMESTEPS_IN_PARALLEL + 1>> neighbor_stencil_md_next_dt;
   std::vector<std::array<class Force*, NUM_TIMESTEPS_IN_PARALLEL + 1>> force_stencil_md_next_dt;
 
-#if defined(_OPENMP)
+#ifdef LMP_OPENMP
   std::vector<std::array<class Modify*, NUM_TIMESTEPS_IN_PARALLEL + 1>> modify_stencil_md_omp;
 #endif
 
