1c24b29e4e089405eea9c5e71e7a76e2385723a7
ryandeng1 <ryandeng@mit.edu>
2024-03-27 22:26:51 -0400

    enable stencil md

--- src/verlet.cpp
+++ src/verlet.cpp
@@ -234,7 +234,7 @@ void Verlet::setup(int flag) {
     update->setupflag = 0;
 
     std::cout << GREEN << "------------------- LAMMPS SETUP DONE -------------------------" << RESET_COLOR << std::endl;
-    // setup_stencil_md();
+    setup_stencil_md();
 }
 
 void atom_reorder_stencil_md(Atom* atom_, int* current, int* permute, int start,
