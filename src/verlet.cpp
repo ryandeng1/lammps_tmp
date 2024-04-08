014d1fc1167ee4ffa72263816ad5fd84735b8805
ryandeng1 <ryandeng@mit.edu>
2024-04-07 21:44:33 -0400

    reparallelize unproftable loops

--- src/verlet.cpp
+++ src/verlet.cpp
@@ -5854,8 +5854,8 @@ void Verlet::run_stencil_md(int starting_timestep, std::map<int, std::vector<int
 
     // clear force on everything except last timestep of initial,
     auto begin_misc = std::chrono::high_resolution_clock::now();
-    // cilk_for (int i = 0; i < NUM_ZOIDS; i++) {
-    for (int i = 0; i < NUM_ZOIDS; i++) {
+    cilk_for (int i = 0; i < NUM_ZOIDS; i++) {
+    // for (int i = 0; i < NUM_ZOIDS; i++) {
         if (i % comm->nprocs == comm->me) {
             cilk_for (int t = 0; t < NUM_TIMESTEPS_IN_PARALLEL; t++) {
                 Atom* atom_ = lmp->atom_stencil_md[i][t];
@@ -6062,7 +6062,7 @@ void Verlet::run_stencil_md(int starting_timestep, std::map<int, std::vector<int
     // this should get optimized to be `memset` with -O3
     begin_misc = std::chrono::high_resolution_clock::now();
     // cilk_for (int i = 0; i < NUM_ZOIDS; i++) {
-    for (int i = 0; i < NUM_ZOIDS; i++) {
+    cilk_for (int i = 0; i < NUM_ZOIDS; i++) {
         if (i % comm->nprocs == comm->me) {
             cilk_for (int t = 1; t < NUM_TIMESTEPS_IN_PARALLEL + 1; t++) {
                 Atom* atom_ = lmp->atom_stencil_md[i][t];
