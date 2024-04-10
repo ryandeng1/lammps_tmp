473eabb2bc4258fc2b8595e53183051942736595
ryandeng1 <ryandeng@mit.edu>
2024-04-10 12:59:47 -0400

    sort neighbor idxs

--- src/OPENMP/npair_half_bin_atomonly_newton_omp.cpp
+++ src/OPENMP/npair_half_bin_atomonly_newton_omp.cpp
@@ -304,12 +304,10 @@ void NPairHalfBinAtomonlyNewtonOmp::build_stencil_md(NeighList *list, Atom* atom
             error->one(FLERR,"Neighbor list overflow, boost neigh_modify one");
 
         // std::cout << "sorting: " << std::is_sorted(neighbors.begin(), neighbors.end()) << std::endl;
-        /*
         std::sort(neighbors.begin(), neighbors.end());
         for (int neigh_idx = 0; neigh_idx < n; neigh_idx++) {
             neighptr[neigh_idx] = neighbors[neigh_idx];
         }
-        */
     }
 
     list->inum = inum;
