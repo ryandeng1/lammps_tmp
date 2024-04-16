26b7e53597f8be196a88b76afffaacfbaf12069a
ryandeng1 <ryandeng@mit.edu>
2024-04-15 20:54:38 -0400

    add pack atom vec unpack

--- src/atom_vec.cpp
+++ src/atom_vec.cpp
@@ -1978,10 +1978,6 @@ void AtomVec::unpack_data_from_process_stencil_md(int nrecv_force, int nrecv_pos
                 local_list_idx += segment_size;
             } else {
                 assert(segment_type == RECV_DATA_PROCESS_GHOST);
-                counter++;
-                counter++;
-                counter++;
-                /*
                 for (int j = 0; j < segment_size; j++) {
                     int buf_idx = counter / 3;
                     double x_x = buf[counter++];
@@ -2001,7 +1997,6 @@ void AtomVec::unpack_data_from_process_stencil_md(int nrecv_force, int nrecv_pos
 
                     ghost_idx++;
                 }
-                */
             }
         }
 
