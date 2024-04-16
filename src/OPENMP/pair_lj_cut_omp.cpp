a9334f547c21ee76c52ace4f2e308c15c5737d29
ryandeng1 <ryandeng@mit.edu>
2024-04-16 12:56:10 -0400

    add back previous change?

--- src/OPENMP/pair_lj_cut_omp.cpp
+++ src/OPENMP/pair_lj_cut_omp.cpp
@@ -111,7 +111,11 @@ void PairLJCutOMP::compute_stencil_md(int eflag, int vflag, Atom* atom_, bool* c
     const int nthreads = comm->nthreads;
     const int inum = list->inum;
 
-    int nthreads_to_use = inum / NUM_WORKERS_PER_THREAD + 1;
+    int nthreads_to_use = inum / NUM_WORKERS_PER_THREAD;
+
+    if (nthreads_to_use < 1) {
+        nthreads_to_use = 1;
+    }
 
     if (nthreads_to_use > nthreads) {
         nthreads_to_use = nthreads;
