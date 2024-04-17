36c622b067b0a3b83d9a5ca9dd46abd47593d9bc
ryandeng1 <ryandeng@mit.edu>
2024-04-16 21:33:41 -0400

    modify keep grainsize

--- src/OPENMP/fix_nve_omp.cpp
+++ src/OPENMP/fix_nve_omp.cpp
@@ -111,6 +111,7 @@ void FixNVEOMP::initial_integrate_stencil_md(int /* vflag */, Atom* atom_, Atom*
         const double * const mass = atom->mass;
         const int * const type = atom_->type;
 
+        #pragma cilk grainsize 128
         cilk_for (int i = 0; i < nlocal; i++) {
             if (mask[i] & groupbit) {
                 const double dtfm = dtf / mass[type[i]];
@@ -199,6 +200,7 @@ void FixNVEOMP::final_integrate_stencil_md(Atom* atom_, Atom* next, Neighbor* ne
         const double * const mass = atom->mass;
         const int * const type = next->type;
 
+        #pragma cilk grainsize 128
         cilk_for (int i = 0; i < nlocal; i++) {
             int next_idx = atom_idx_mapping[i];
             assert(next_idx != -1);
@@ -207,6 +209,7 @@ void FixNVEOMP::final_integrate_stencil_md(Atom* atom_, Atom* next, Neighbor* ne
             next_v[next_idx].z = v[i].z;
         }
 
+        #pragma cilk grainsize 128
         cilk_for (int i = 0; i < next_nlocal; i++) {
             if (mask[i] & groupbit) {
                 const double dtfm = dtf / mass[type[i]];
