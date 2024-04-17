a57ca7ccd27d5bc1ddca1a038bc2f7e30e9e7a2e
ryandeng1 <ryandeng@mit.edu>
2024-04-16 20:50:09 -0400

    only run stencilmd

--- src/stencil_md_utils.h
+++ src/stencil_md_utils.h
@@ -79,7 +79,7 @@ constexpr int NUM_WORKERS_PER_THREAD = 128;
 
 constexpr bool ONLY_RUN_LAMMPS = false;
 
-constexpr bool ONLY_RUN_STENCIL_MD = false;
+constexpr bool ONLY_RUN_STENCIL_MD = true;
 
 const std::map<std::tuple<int, int, int>, int> zoid_to_num_map = {
         {std::make_tuple(LEFT, LEFT, LEFT), 0},
