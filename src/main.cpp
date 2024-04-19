41518d2f6a3cc744f51b2a73ea02d522eef3e5ce
ryandeng1 <ryandeng@mit.edu>
2024-04-19 19:30:39 -0400

    thread multiple

--- src/main.cpp
+++ src/main.cpp
@@ -46,9 +46,9 @@ int main(int argc, char **argv)
   // MPI_Init(&argc, &argv);
   // TODO: Ryan stencil md, change this back if needed?
   int provided;
-  MPI_Init_thread(&argc, &argv, MPI_THREAD_SERIALIZED, &provided);
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
   assert(provided >= MPI_THREAD_MULTIPLE);
-  if (provided < MPI_THREAD_SERIALIZED) {
+  if (provided < MPI_THREAD_MULTIPLE) {
     std::cout << "COULD NOT PROVIDE MPI_THREAD_MULTIPLE" << std::endl;
     return 0;
   }
