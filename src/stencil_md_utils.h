879f64b1c3748d2b9e82e609f5aaf233b64d7572
ryandeng1 <ryandeng@mit.edu>
2024-04-18 21:42:57 -0400

    lammps use cilk

--- src/stencil_md_utils.h
+++ src/stencil_md_utils.h
@@ -81,7 +81,7 @@ constexpr bool ONLY_RUN_LAMMPS = true;
 
 constexpr bool ONLY_RUN_STENCIL_MD = false;
 
-constexpr bool LAMMPS_USE_CILK = false;
+constexpr bool LAMMPS_USE_CILK = true;
 
 const std::map<std::tuple<int, int, int>, int> zoid_to_num_map = {
         {std::make_tuple(LEFT, LEFT, LEFT), 0},
