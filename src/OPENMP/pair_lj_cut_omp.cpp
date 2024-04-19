c0cd29c6bd12328ca907e0b3430c1d8223ef63c5
ryandeng1 <ryandeng@mit.edu>
2024-04-18 22:48:51 -0400

    move up cilkscale

--- src/OPENMP/pair_lj_cut_omp.cpp
+++ src/OPENMP/pair_lj_cut_omp.cpp
@@ -96,6 +96,11 @@ void PairLJCutOMP::compute(int eflag, int vflag)
           thr->timer(Timer::PAIR);
       } // end of omp parallel region
 
+      wsp_t end = wsp_getworkspan();
+      wsp_t elapsed = wsp_sub(end, start);
+      wsp_dump(elapsed, "potential_calc");
+
+
       // try new reduce
 
       if (comm->nthreads == 1) {
@@ -114,10 +119,6 @@ void PairLJCutOMP::compute(int eflag, int vflag)
           }
           f[i] = t0;
       }
-
-      wsp_t end = wsp_getworkspan();
-      wsp_t elapsed = wsp_sub(end, start);
-      wsp_dump(elapsed, "potential_calc");
 
       return;
   }
