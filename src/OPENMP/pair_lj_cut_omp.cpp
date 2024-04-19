e2bbee117f2af3701e3da3495ede38737046807b
ryandeng1 <ryandeng@mit.edu>
2024-04-19 19:36:18 -0400

    grainsize reduce

--- src/OPENMP/pair_lj_cut_omp.cpp
+++ src/OPENMP/pair_lj_cut_omp.cpp
@@ -201,6 +201,7 @@ void PairLJCutOMP::compute(int eflag, int vflag)
       int nworkers = __cilkrts_get_nworkers();
 
       // wsp_t start_reduce = wsp_getworkspan();
+      #pragma cilk grainsize 16
       cilk_for (int i = 0; i < nvals; i++) {
           /*
           cilk::opadd_reducer<double> t0 = f[i];
