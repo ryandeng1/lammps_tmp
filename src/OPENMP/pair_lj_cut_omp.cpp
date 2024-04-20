64f35a01567db76a15dd5558d452182fdb3801a3
ryandeng1 <ryandeng@mit.edu>
2024-04-20 12:09:05 -0400

    test no reduce

--- src/OPENMP/pair_lj_cut_omp.cpp
+++ src/OPENMP/pair_lj_cut_omp.cpp
@@ -432,6 +432,7 @@ void PairLJCutOMP::compute_stencil_md(int eflag, int vflag, Atom* atom_, bool* c
 
     constexpr int CHUNK_SIZE = 256;
 
+    /*
     cilk_for (int i = 0; i < nvals; i += CHUNK_SIZE) {
         for (int n = 1; n < nthreads_to_use; n++) {
             for (int j = i; j < nvals && j < i + CHUNK_SIZE; j++) {
@@ -439,6 +440,7 @@ void PairLJCutOMP::compute_stencil_md(int eflag, int vflag, Atom* atom_, bool* c
             }
         }
     }
+    */
 
     /*
     // #pragma cilk grainsize NUM_WORKERS_PER_THREAD
