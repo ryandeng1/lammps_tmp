8c1112735357616b61fec1e3960c70736a2ecfac
ryandeng1 <ryandeng@mit.edu>
2024-04-19 20:54:43 -0400

    fix

--- src/OPENMP/pair_lj_cut_omp.cpp
+++ src/OPENMP/pair_lj_cut_omp.cpp
@@ -208,7 +208,7 @@ void PairLJCutOMP::compute(int eflag, int vflag)
       cilk_for (int i = 0; i < nvals; i += CHUNK_SIZE) {
           for (int n = 1; n < nworkers; n++) {
               for (int j = i; j < nvals && j < i + CHUNK_SIZE; j++) {
-                  f[i] += f[n * nvals + i];
+                  f[j] += f[n * nvals + j];
               }
           }
       }
