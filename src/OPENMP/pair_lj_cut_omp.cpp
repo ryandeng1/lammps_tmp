f35ffcf372870d45ffc4937880ac9ebc5e18a5a4
ryandeng1 <ryandeng@mit.edu>
2024-04-19 20:07:44 -0400

    grainsize 64 on compute

--- src/OPENMP/pair_lj_cut_omp.cpp
+++ src/OPENMP/pair_lj_cut_omp.cpp
@@ -69,6 +69,7 @@ void PairLJCutOMP::compute(int eflag, int vflag)
 
       bool newton_pair = force->newton_pair;
 
+      #pragma cilk grainsize 64
       cilk_for (int i = 0; i < atom->nlocal; i++) {
           int tid = __cilkrts_get_worker_number();
           ThrData *thr = fix->get_thr(tid);
