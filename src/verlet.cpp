430da4d4aa81c0f1a8dbd57b8b66e95e89b9466d
ryandeng1 <ryandeng@mit.edu>
2024-04-29 19:44:24 -0400

    memset force array grainsize 1

--- src/verlet.cpp
+++ src/verlet.cpp
@@ -6194,8 +6194,10 @@ void Verlet::run_stencil_md(int starting_timestep, std::vector<int>* dep_to_wait
     // clear force on everything except first timestep
     // this should get optimized to be `memset` with -O3
     begin_misc = std::chrono::high_resolution_clock::now();
+    #pragma cilk grainsize 1
     cilk_for (int i = 0; i < NUM_ZOIDS; i++) {
         if (i % comm->nprocs == comm->me) {
+            #pragma cilk grainsize 1
             cilk_for (int t = 1; t < NUM_TIMESTEPS_IN_PARALLEL + 1; t++) {
                 Atom* atom_ = lmp->atom_stencil_md[i][t];
                 int nall = atom_->nlocal + atom_->nghost;
