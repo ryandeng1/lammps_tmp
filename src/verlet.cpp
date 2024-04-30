d8709ca4cc759b469cdba04199fe149d4fe41fdf
ryandeng1 <ryandeng@mit.edu>
2024-04-30 19:24:43 -0400

    more optimized implementation

--- src/verlet.cpp
+++ src/verlet.cpp
@@ -5981,14 +5981,12 @@ void Verlet::run_stencil_md(int starting_timestep, std::vector<int>* dep_to_wait
 
     // clear force on everything except last timestep of initial,
     auto begin_misc = std::chrono::high_resolution_clock::now();
-    cilk_for (int i = 0; i < NUM_ZOIDS; i++) {
-        if (i % comm->nprocs == comm->me) {
-            cilk_for (int t = 0; t < NUM_TIMESTEPS_IN_PARALLEL; t++) {
-                Atom* atom_ = lmp->atom_stencil_md[i][t];
-                int nall = atom_->nlocal + atom_->nghost;
-                memset(&atom_->f[0][0], 0, (nall) * 3 * sizeof(double));
-                memset(&atom_->eval_f_stencil_md[0][0], 0, (nall) * 3 * sizeof(double));
-            }
+    cilk_for (int i = comm->me; i < NUM_ZOIDS; i += comm->nprocs) {
+        cilk_for (int t = 0; t < NUM_TIMESTEPS_IN_PARALLEL; t++) {
+            Atom* atom_ = lmp->atom_stencil_md[i][t];
+            int nall = atom_->nlocal + atom_->nghost;
+            memset(&atom_->f[0][0], 0, (nall) * 3 * sizeof(double));
+            memset(&atom_->eval_f_stencil_md[0][0], 0, (nall) * 3 * sizeof(double));
         }
     }
 
@@ -6200,14 +6198,12 @@ void Verlet::run_stencil_md(int starting_timestep, std::vector<int>* dep_to_wait
     // clear force on everything except first timestep
     // this should get optimized to be `memset` with -O3
     begin_misc = std::chrono::high_resolution_clock::now();
-    cilk_for (int i = 0; i < NUM_ZOIDS; i++) {
-        if (i % comm->nprocs == comm->me) {
-            cilk_for (int t = 1; t < NUM_TIMESTEPS_IN_PARALLEL + 1; t++) {
-                Atom* atom_ = lmp->atom_stencil_md[i][t];
-                int nall = atom_->nlocal + atom_->nghost;
-                memset(&atom_->f[0][0], 0, (nall) * 3 * sizeof(double));
-                memset(&atom_->eval_f_stencil_md[0][0], 0, (nall) * 3 * sizeof(double));
-            }
+    cilk_for (int i = comm->me; i < NUM_ZOIDS; i += comm->nprocs) {
+        cilk_for (int t = 1; t < NUM_TIMESTEPS_IN_PARALLEL + 1; t++) {
+            Atom* atom_ = lmp->atom_stencil_md[i][t];
+            int nall = atom_->nlocal + atom_->nghost;
+            memset(&atom_->f[0][0], 0, (nall) * 3 * sizeof(double));
+            memset(&atom_->eval_f_stencil_md[0][0], 0, (nall) * 3 * sizeof(double));
         }
     }
 
