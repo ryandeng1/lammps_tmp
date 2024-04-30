39404a23cacbeaeccd2a56edecab6f8e7a873b70
ryandeng1 <ryandeng@mit.edu>
2024-04-30 12:41:07 -0400

    workers per chunk 256

--- src/stencil_md_utils.h
+++ src/stencil_md_utils.h
@@ -75,7 +75,7 @@ constexpr bool TRY_PRECOMPUTE_RELEVANT_ATOM_IDX = false;
 
 constexpr bool DEBUG = true;
 
-constexpr int NUM_WORKERS_PER_THREAD = 128;
+constexpr int NUM_WORKERS_PER_THREAD = 256;
 
 constexpr bool ONLY_RUN_LAMMPS = false;
 
