e54fcc2ece3963fc4ea648b7fb38134a06c5dffb
ryandeng1 <ryandeng@mit.edu>
2024-04-30 22:08:51 -0400

    32 --> 64

--- src/stencil_md_utils.h
+++ src/stencil_md_utils.h
@@ -75,7 +75,7 @@ constexpr bool TRY_PRECOMPUTE_RELEVANT_ATOM_IDX = false;
 
 constexpr bool DEBUG = true;
 
-constexpr int NUM_WORKERS_PER_THREAD = 32;
+constexpr int NUM_WORKERS_PER_THREAD = 64;
 
 constexpr bool ONLY_RUN_LAMMPS = false;
 
