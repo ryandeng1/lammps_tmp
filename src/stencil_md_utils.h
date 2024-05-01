916bad3cd2f1fb6da35e4c55f67dabff3716a014
ryandeng1 <ryandeng@mit.edu>
2024-04-30 22:34:51 -0400

    lammps do not use cilk

--- src/stencil_md_utils.h
+++ src/stencil_md_utils.h
@@ -81,7 +81,7 @@ constexpr bool ONLY_RUN_LAMMPS = true;
 
 constexpr bool ONLY_RUN_STENCIL_MD = false;
 
-constexpr bool LAMMPS_USE_CILK = true;
+constexpr bool LAMMPS_USE_CILK = false;
 
 constexpr bool TIME_STENCIL_MD = true;
 
