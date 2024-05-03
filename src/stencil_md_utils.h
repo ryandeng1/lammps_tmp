a04a1b60c67eeb36d42d3bf895e9058141c6cda9
ryandeng1 <ryandeng@mit.edu>
2024-05-03 10:32:55 -0400

    disable timing

--- src/stencil_md_utils.h
+++ src/stencil_md_utils.h
@@ -83,7 +83,7 @@ constexpr bool ONLY_RUN_STENCIL_MD = true;
 
 constexpr bool LAMMPS_USE_CILK = false;
 
-constexpr bool TIME_STENCIL_MD = true;
+constexpr bool TIME_STENCIL_MD = false;
 
 const std::map<std::tuple<int, int, int>, int> zoid_to_num_map = {
         {std::make_tuple(LEFT, LEFT, LEFT), 0},
