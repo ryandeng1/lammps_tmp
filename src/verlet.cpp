9d1b3f39a4a7bb5f2c0b728a2f6138daecadd3e1
ryandeng1 <ryandeng@mit.edu>
2024-05-03 09:18:01 -0400

    stencilmd disable printing of ratios

--- src/verlet.cpp
+++ src/verlet.cpp
@@ -5312,6 +5312,7 @@ void Verlet::run(int n) {
     */
 
     if (TIME_STENCIL_MD) {
+        /*
         for (int zoid_num = 0; zoid_num < NUM_ZOIDS; zoid_num++) {
             if (zoid_num % comm->nprocs == comm->me) {
                 for (int t = 1; t < NUM_TIMESTEPS_IN_PARALLEL + 1; t++) {
@@ -5330,6 +5331,7 @@ void Verlet::run(int n) {
 
             MPI_Barrier(world);
         }
+        */
     }
 
     int64_t stencil_md_total_send_comm_duration = 0;
