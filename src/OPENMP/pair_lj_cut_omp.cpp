a2beeef2a07222e7df1265a0bf78a90f35a0f6d0
ryandeng1 <ryandeng@mit.edu>
2024-05-03 09:33:06 -0400

    commnet out lammps openmp code even if it never gets called

--- src/OPENMP/pair_lj_cut_omp.cpp
+++ src/OPENMP/pair_lj_cut_omp.cpp
@@ -175,6 +175,7 @@ void PairLJCutOMP::compute(int eflag, int vflag)
       return;
   }
 
+  /*
 #if defined(_OPENMP)
 #pragma omp parallel LMP_DEFAULT_NONE LMP_SHARED(eflag,vflag)
 #endif
@@ -202,6 +203,7 @@ void PairLJCutOMP::compute(int eflag, int vflag)
     reduce_thr(this, eflag, vflag, thr);
   } // end of omp parallel region
   // std::cout << "num lammps edges: " << num_lammps_edges << std::endl;
+  */
 }
 
 void PairLJCutOMP::compute_stencil_md(int eflag, int vflag, Atom* atom_, bool* can_eval_center, queue_info& zoid, int* num_eval) {
