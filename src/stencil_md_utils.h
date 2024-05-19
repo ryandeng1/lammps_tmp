ddfe227671497aec24ab0f5174f9e6333f6d1512
ryandeng1 <ryandeng@mit.edu>
2024-05-19 16:38:59 -0400

    only run stencilmd

--- src/stencil_md_utils.h
+++ src/stencil_md_utils.h
@@ -77,11 +77,11 @@ constexpr bool DEBUG = true;
 
 constexpr int NUM_WORKERS_PER_THREAD = 64;
 
-constexpr bool ONLY_RUN_LAMMPS = true;
+constexpr bool ONLY_RUN_LAMMPS = false;
 
-constexpr bool ONLY_RUN_STENCIL_MD = false;
+constexpr bool ONLY_RUN_STENCIL_MD = true;
 
-constexpr bool LAMMPS_USE_CILK = false;
+constexpr bool LAMMPS_USE_CILK = true;
 
 constexpr bool TIME_STENCIL_MD = true;
 
