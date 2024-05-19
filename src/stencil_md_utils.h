dd1f56cf1e20894f7bac0dbac776b4cce0ea9cee
ryandeng1 <ryandeng@mit.edu>
2024-05-19 17:53:43 -0400

    only run stencilmd dt=3

--- src/stencil_md_utils.h
+++ src/stencil_md_utils.h
@@ -54,7 +54,7 @@ constexpr int NUM_DEPS = 4;
 
 constexpr int NUM_ZOIDS = 4 * 4 * 4;
 
-constexpr int NUM_TIMESTEPS_IN_PARALLEL = 4;
+constexpr int NUM_TIMESTEPS_IN_PARALLEL = 3;
 constexpr double ADDITIONAL_CUTOFF = 0.4;
 
 constexpr double ALLEGRO_CUTOFF_RADIUS = 1.12;
@@ -77,11 +77,11 @@ constexpr bool DEBUG = true;
 
 constexpr int NUM_WORKERS_PER_THREAD = 64;
 
-constexpr bool ONLY_RUN_LAMMPS = true;
+constexpr bool ONLY_RUN_LAMMPS = false;
 
-constexpr bool ONLY_RUN_STENCIL_MD = false;
+constexpr bool ONLY_RUN_STENCIL_MD = true;
 
-constexpr bool LAMMPS_USE_CILK = false;
+constexpr bool LAMMPS_USE_CILK = true;
 
 constexpr bool TIME_STENCIL_MD = true;
 
