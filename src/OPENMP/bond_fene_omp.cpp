5a25a393808cc0e67430103d49fe2ca3c63c2533
ryandeng1 <ryandeng@mit.edu>
2024-07-30 22:53:02 -0400

    add serial base case

--- src/OPENMP/bond_fene_omp.cpp
+++ src/OPENMP/bond_fene_omp.cpp
@@ -230,6 +230,84 @@ void BondFENEOMP::compute_stencil_md(int eflag, int vflag, Atom* atom_, bool* ca
         return;
     }
 
+    if (nlocal < 4096) {
+        const auto * _noalias const x = (dbl3_t *) atom_->x[0];
+        auto * _noalias const f = (dbl3_t *) atom_->eval_f_stencil_md[0];
+        const int3_t * _noalias const bondlist = (int3_t *) neighbor_->bondlist[0];
+        double ebond = 0.0;
+
+        for (int n = 0; n < inum; n++) {
+            int i1 = bondlist[n].a;
+            int i2 = bondlist[n].b;
+            int type = bondlist[n].t;
+
+            double delx = x[i1].x - x[i2].x;
+            double dely = x[i1].y - x[i2].y;
+            double delz = x[i1].z - x[i2].z;
+
+            double rsq = delx*delx + dely*dely + delz*delz;
+            double r0sq = r0[type] * r0[type];
+            double rlogarg = 1.0 - rsq/r0sq;
+
+            // if r -> r0, then rlogarg < 0.0 which is an error
+            // issue a warning and reset rlogarg = epsilon
+            // if r > 2*r0 something serious is wrong, abort
+
+            if (rlogarg < 0.1) {
+                error->warning(FLERR,"FENE bond too long: {} {} {} {:.8}",
+                               update->ntimestep,atom->tag[i1],atom->tag[i2],sqrt(rsq));
+                /*
+                if (check_error_thr((rlogarg <= -3.0),tid,FLERR,"Bad FENE bond"))
+                    return;
+                */
+                assert(false);
+
+                rlogarg = 0.1;
+            }
+
+            double fbond = -k[type]/rlogarg;
+
+            // force from LJ term
+            double sr2 = 0.0;
+            double sr6 = 0.0;
+
+            if (rsq < MY_CUBEROOT2*sigma[type]*sigma[type]) {
+                sr2 = sigma[type]*sigma[type]/rsq;
+                sr6 = sr2*sr2*sr2;
+                fbond += 48.0*epsilon[type]*sr6*(sr6-0.5)/rsq;
+            }
+
+            // energy
+
+            if (eflag) {
+                ebond = -0.5 * k[type]*r0sq*log(rlogarg);
+                if (rsq < MY_CUBEROOT2*sigma[type]*sigma[type])
+                    ebond += 4.0*epsilon[type]*sr6*(sr6-1.0) + epsilon[type];
+            }
+
+            // apply force to each of 2 atoms
+
+            if (newton || i1 < nlocal) {
+                f[i1].x += delx*fbond;
+                f[i1].y += dely*fbond;
+                f[i1].z += delz*fbond;
+            }
+
+            if (newton || i2 < nlocal) {
+                f[i2].x -= delx*fbond;
+                f[i2].y -= dely*fbond;
+                f[i2].z -= delz*fbond;
+            }
+
+            /*
+            if (evflag) ev_tally_thr(this,i1,i2,nlocal,newton,
+                                     ebond,fbond,delx,dely,delz,thr);
+            */
+        }
+
+        return;
+    }
+
     if (PAIR_USE_BINS) {
         const auto * _noalias const x = (dbl3_t *) atom_->x[0];
         auto * _noalias const f = (dbl3_t *) atom_->eval_f_stencil_md[0];
--- src/OPENMP/pair_lj_cut_omp.cpp
+++ src/OPENMP/pair_lj_cut_omp.cpp
@@ -306,6 +306,93 @@ void PairLJCutOMP::compute_stencil_md(int eflag, int vflag, Atom* atom_, bool* c
         return;
     }
 
+    if (nlocal < 4096) {
+        const auto * _noalias const x = (dbl3_t *) atom_->x[0];
+        auto * _noalias const f = (dbl3_t *) atom_->eval_f_stencil_md[0];
+        const int * _noalias const type = atom_->type;
+        const double * _noalias const special_lj = force->special_lj;
+        const int * _noalias const ilist = list->ilist;
+        const int * _noalias const numneigh = list->numneigh;
+        const int * const * const firstneigh = list->firstneigh;
+
+        // loop over neighbors of my atoms
+        for (int ii = 0; ii < nlocal; ii++) {
+            const int i = ilist[ii];
+            const int itype = type[i];
+
+            const int    * _noalias const jlist = firstneigh[i];
+            const double * _noalias const cutsqi = cutsq[itype];
+            const double * _noalias const offseti = offset[itype];
+            const double * _noalias const lj1i = lj1[itype];
+            const double * _noalias const lj2i = lj2[itype];
+            const double * _noalias const lj3i = lj3[itype];
+            const double * _noalias const lj4i = lj4[itype];
+
+            double xtmp = x[i].x;
+            double ytmp = x[i].y;
+            double ztmp = x[i].z;
+            int jnum = numneigh[i];
+
+            double fxtmp = 0.0;
+            double fytmp = 0.0;
+            double fztmp = 0.0;
+
+            for (int jj = 0; jj < jnum; jj++) {
+                double evdwl = 0.0;
+                int j = jlist[jj];
+                double factor_lj = special_lj[sbmask(j)];
+                j &= NEIGHMASK;
+
+                double delx = xtmp - x[j].x;
+                double dely = ytmp - x[j].y;
+                double delz = ztmp - x[j].z;
+                double rsq = delx*delx + dely*dely + delz*delz;
+                int jtype = type[j];
+
+                if (rsq < cutsqi[jtype]) {
+                    double r2inv = 1.0/rsq;
+                    double r6inv = r2inv*r2inv*r2inv;
+                    double forcelj = r6inv * (lj1i[jtype]*r6inv - lj2i[jtype]);
+                    double fpair = factor_lj*forcelj*r2inv;
+
+                    fxtmp += delx*fpair;
+                    fytmp += dely*fpair;
+                    fztmp += delz*fpair;
+
+                    /*
+                    ftmp.x += delx*fpair;
+                    ftmp.y += dely*fpair;
+                    ftmp.z += delz*fpair;
+                    */
+
+                    if (newton_pair || j < nlocal) {
+                        f[j].x -= delx*fpair;
+                        f[j].y -= dely*fpair;
+                        f[j].z -= delz*fpair;
+                    }
+
+                    /*
+                    if (EFLAG) {
+                        evdwl = r6inv*(lj3i[jtype]*r6inv-lj4i[jtype]) - offseti[jtype];
+                        evdwl *= factor_lj;
+                    }
+
+                    if (EVFLAG) {
+                        ev_tally_thr(this, i, j, nlocal, NEWTON_PAIR,
+                                     evdwl, 0.0, fpair, delx, dely, delz, thr);
+                    }
+                    */
+                }
+            }
+
+            f[i].x += fxtmp;
+            f[i].y += fytmp;
+            f[i].z += fztmp;
+        }
+
+        return;
+    }
+
     if (PAIR_USE_BINS) {
         const auto * _noalias const x = (dbl3_t *) atom_->x[0];
         auto * _noalias const f = (dbl3_t *) atom_->eval_f_stencil_md[0];
