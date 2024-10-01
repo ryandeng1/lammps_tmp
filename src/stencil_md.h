d24534d84918f37bed6f2291e607a975c767caa9
ryandeng1 <ryandeng@mit.edu>
2024-10-01 19:50:09 -0400

    post force grainsize 512

--- src/stencil_md.h
+++ src/stencil_md.h
@@ -1908,7 +1908,7 @@ public:
 
         const int nlocal = atom_->nlocal;
 
-        #pragma cilk grainsize 1024
+        #pragma cilk grainsize 512
         cilk_for (int i = 0; i < nlocal; i++) {
             // these are per-atom variables that get updated. Need to put them here to avoid races.
             // double fdrag[3],fran[3];
