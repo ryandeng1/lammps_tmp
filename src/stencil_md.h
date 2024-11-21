93e70e688fca2fab7411ec4b033ca3ac84452b03
ryandeng1 <ryandeng@mit.edu>
2024-11-21 17:22:07 -0500

    revert back to old loop, amkes things more clear

--- src/stencil_md.h
+++ src/stencil_md.h
@@ -2013,7 +2013,11 @@ public:
             for (int c = 0; c < num_chunks; ++c) {
                 int s = (c + start_chunk) % num_chunks;
 
-                if (!claimed_flag[s].test(std::memory_order_relaxed) && !claimed_flag[s].test_and_set(std::memory_order_relaxed)) {
+                if (claimed_flag[s].test(std::memory_order_relaxed)) {
+                    continue;
+                }
+
+                if (!claimed_flag[s].test_and_set(std::memory_order_relaxed)) {
                     for (int i = s * MODIFY_GRAINSIZE; i < (s + 1) * MODIFY_GRAINSIZE && i < next_nlocal; i++) {
                         // for (int i = s * chunk_size; i < (s + 1) * chunk_size && i < next_nlocal; i++) {
                         const double dtfm = local_dtfm[i];
@@ -2572,7 +2576,11 @@ public:
             for (int c = 0; c < num_chunks; ++c) {
                 int s = (c + start_chunk) % num_chunks;
 
-                if (!claimed_flag[s].test(std::memory_order_relaxed) && !claimed_flag[s].test_and_set(std::memory_order_relaxed)) {
+                if (claimed_flag[s].test(std::memory_order_relaxed)) {
+                    continue;
+                }
+
+                if (!claimed_flag[s].test_and_set(std::memory_order_relaxed)) {
                     for (int i = s * MODIFY_GRAINSIZE; i < (s + 1) * MODIFY_GRAINSIZE && i < nlocal; i++) {
                     // for (int i = s * chunk_size; i < (s + 1) * chunk_size && i < nlocal; i++) {
                         const double dtfm = local_dtfm[i];
@@ -3484,8 +3492,7 @@ public:
             for (int c = 0; c < num_chunks; ++c) {
                 int s = (c + start_chunk) % num_chunks;
 
-                // if (!claimed_flag[s].test(std::memory_order_relaxed) && !claimed_flag[s].test_and_set(std::memory_order_relaxed)) {
-                if (claimed_flag[s].test()) {
+                if (claimed_flag[s].test(std::memory_order_relaxed)) {
                     continue;
                 }
 
