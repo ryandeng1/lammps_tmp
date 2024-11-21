e74cb6043b763678ddadbe4b4ff03078498f77c8
ryandeng1 <ryandeng@mit.edu>
2024-11-21 14:41:43 -0500

    revert back to fixed grainsize

--- src/stencil_md.cpp
+++ src/stencil_md.cpp
@@ -1293,7 +1293,7 @@ void StencilMD::SET_CLAIMED_ATOMIC_BOOLS() {
                 int zoid_num = zoid.num;
                 if (zoid_num % comm->nprocs == comm->me) {
                     Atom* atom_ = lmp->atom_stencil_md[zoid_num][t];
-                    int total = atom_->nlocal + atom_->nghost;
+                    int total = atom_->nlocal;
                     int num_chunks = total / MODIFY_GRAINSIZE + 1;
                     // int num_chunks = __cilkrts_get_nworkers();
                     atom_->claimed = new std::atomic<bool>[num_chunks];
