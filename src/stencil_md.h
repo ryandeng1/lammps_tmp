2f6fd3f3c62d3b0f71628a0ad94c304213aeda54
ryandeng1 <ryandeng@mit.edu>
2024-12-20 14:04:24 -0500

    TESTING ONLY eliminate eval_f, only use f

--- src/stencil_md.h
+++ src/stencil_md.h
@@ -4436,16 +4436,19 @@ public:
                         int i = local_idxs[idx];
 
                         const double dtfm = dtf / mass[type[i]];
-                        v[i].x += dtfm * (f[i].x + eval_f[i].x);
-                        v[i].y += dtfm * (f[i].y + eval_f[i].y);
-                        v[i].z += dtfm * (f[i].z + eval_f[i].z);
+                        // v[i].x += dtfm * (f[i].x + eval_f[i].x);
+                        // v[i].y += dtfm * (f[i].y + eval_f[i].y);
+                        // v[i].z += dtfm * (f[i].z + eval_f[i].z);
+                        v[i].x += dtfm * (f[i].x);
+                        v[i].y += dtfm * (f[i].y);
+                        v[i].z += dtfm * (f[i].z);
 
                         f[i].x = 0.0;
                         f[i].y = 0.0;
                         f[i].z = 0.0;
-                        eval_f[i].x = 0.0;
-                        eval_f[i].y = 0.0;
-                        eval_f[i].z = 0.0;
+                        // eval_f[i].x = 0.0;
+                        // eval_f[i].y = 0.0;
+                        // eval_f[i].z = 0.0;
 
                         next_x[i].x = x[i].x + dtv * v[i].x;
                         next_x[i].y = x[i].y + dtv * v[i].y;
