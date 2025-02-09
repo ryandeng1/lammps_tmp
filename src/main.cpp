0d6ac55d3f5faf413203eaba742276c5f83a8321
ryandeng1 <ryandeng@mit.edu>
2025-02-09 13:53:55 -0500

    add in this shit to set work affinity

--- src/main.cpp
+++ src/main.cpp
@@ -95,6 +95,29 @@ int main(int argc, char **argv)
         set_worker_affinity(nworkers, cpusets, calling_thread);
 
         delete[] cpusets;
+    } else {
+        auto calling_thread = pthread_self();
+
+        cilk_for (int i = 0; i < 1e6; i++) {
+            if (rand() == 0) {
+                std::cout << "i: " << i << std::endl;
+            }
+        }
+
+        int nworkers = __cilkrts_get_nworkers();
+
+        auto* cpusets = new cpu_set_t[nworkers];
+
+        constexpr int NUM_CORES_PER_SOCKET = 24;
+
+        for (int i = 0; i < nworkers; i++) {
+            CPU_ZERO(&cpusets[i]);
+            CPU_SET(i + NUM_CORES_PER_SOCKET, &cpusets[i]);
+        }
+
+        set_worker_affinity(nworkers, cpusets, calling_thread);
+
+        delete[] cpusets;
     }
 #endif
 
