d6a84dac66d8939256c7a177e9ddd9194c3aa3dc
ryandeng1 <ryandeng@mit.edu>
2025-02-18 13:52:20 -0500

    parallelize packing and sending

--- src/stencil_md.h
+++ src/stencil_md.h
@@ -11349,7 +11349,7 @@ public:
                                           : send_to_neighbors_not_my_proc_idxs_next_dt[zoid_num];
 
         // cilk_for (int i = 0; i < send_neighbors.size(); i++) {
-        for (int i = 0; i < send_neighbors.size(); i++) {
+        cilk_for (int i = 0; i < send_neighbors.size(); i++) {
             int send_zoid_num = send_neighbors[i];
             int nsend = curr_dt ? send_zoid_to_zoid_sizes[zoid_num][i] : send_zoid_to_zoid_sizes_next_dt[zoid_num][i];
             int zoid_ndoubles_send = DEBUG_SEND_RECV_DATA ? nsend * (3 + 1) : nsend * 3;
@@ -11407,7 +11407,7 @@ public:
                                           : send_to_neighbors_not_my_proc_idxs_next_dt[zoid_num];
 
         // cilk_for (int i = 0; i < send_neighbors.size(); i++) {
-        for (int i = 0; i < send_neighbors.size(); i++) {
+        cilk_for (int i = 0; i < send_neighbors.size(); i++) {
             int send_zoid_num = send_neighbors[i];
             int nsend = curr_dt ? send_zoid_to_zoid_sizes[zoid_num][i] : send_zoid_to_zoid_sizes_next_dt[zoid_num][i];
             int zoid_ndoubles_send = DEBUG_SEND_RECV_DATA ? nsend * (3 + 1) : nsend * 3;
