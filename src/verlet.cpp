38f86c2df7bc353669af2c73df36edb8f9352222
ryandeng1 <ryandeng@mit.edu>
2025-03-27 00:41:18 -0400

    remove bad llm code, also try waitany

--- src/verlet.cpp
+++ src/verlet.cpp
@@ -59,6 +59,7 @@
 #include <sstream>
 #include <cilk/opadd_reducer.h>
 #include <iomanip>
+#include <sys/time.h>
 
 using namespace LAMMPS_NS;
 
@@ -8294,12 +8295,12 @@ void Verlet::run_stencil_md_many_cuts_new_comm(int starting_timestep, double **t
 
 void Verlet::run_stencil_md_many_cuts(int num_timesteps, double** test_f, double** test_x, double** test_v) {
     for (int t = 0; t < num_timesteps; t += 2 * NUM_TIMESTEPS_IN_PARALLEL) {
-        run_stencil_md_many_cuts_new_comm<true>(t, test_f, test_x, test_v);
-        run_stencil_md_many_cuts_new_comm<false>(t, test_f, test_x, test_v);
+        // run_stencil_md_many_cuts_new_comm<true>(t, test_f, test_x, test_v);
+        // run_stencil_md_many_cuts_new_comm<false>(t, test_f, test_x, test_v);
         // run_stencil_md_many_cuts_helper<true>(t, test_f, test_x, test_v);
         // run_stencil_md_many_cuts_helper<false>(t, test_f, test_x, test_v);
-        // run_stencil_md_many_cuts_waitany<true>(t, test_f, test_x, test_v);
-        // run_stencil_md_many_cuts_waitany<false>(t, test_f, test_x, test_v);
+        run_stencil_md_many_cuts_waitany<true>(t, test_f, test_x, test_v);
+        run_stencil_md_many_cuts_waitany<false>(t, test_f, test_x, test_v);
     }
 }
 
