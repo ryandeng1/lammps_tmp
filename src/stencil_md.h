b54a33f78153612aeceeda12dfd7251942ffdf62
ryandeng1 <ryandeng@mit.edu>
2025-05-20 15:08:49 -0400

    Revert "try this"
    This reverts commit 6eacd14a47893590b823377fcdebd3a0dc72e13a.

--- src/stencil_md.h
+++ src/stencil_md.h
@@ -10639,7 +10639,6 @@ class StencilMD : protected Pointers {
                   continue;
               }
 
-              /*
               bool keep = false;
 
               for (int dim = 0; dim < domain->dimension; dim++) {
@@ -10652,7 +10651,6 @@ class StencilMD : protected Pointers {
               if (!keep && !is_local) {
                   continue;
               }
-              */
 
               // find zoid that had it previously
               for (int j = 0; j < recv_neighbors.size(); j++) {
