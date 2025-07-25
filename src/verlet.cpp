bc7425940648246b5767d56e6cc370fbcfad0479
ryandeng1 <ryandeng@mit.edu>
2025-07-24 23:06:12 -0700

    bug fix

--- src/verlet.cpp
+++ src/verlet.cpp
@@ -3324,8 +3324,8 @@ void Verlet::run_stencil_md_many_cuts_proc_to_proc(int starting_timestep, double
     std::vector<std::vector<int>> dep_to_active_streams(NUM_DEPS);
     for (int dep = 1; dep < NUM_DEPS; dep++) {
         for (int stream_num = 0; stream_num < NUM_STREAMS; stream_num++) {
-            auto& zoid_pairs_at_stream = stencilMD->stream_num_to_zoid_pairs[curr_dt_idx][dep + 1][stream_num];
-            auto& send_dep_proc_pairs_at_stream = stencilMD->stream_num_to_dep_proc_pairs[curr_dt_idx][dep + 1][stream_num];
+            auto& zoid_pairs_at_stream = stencilMD->stream_num_to_zoid_pairs[curr_dt_idx][dep][stream_num];
+            auto& send_dep_proc_pairs_at_stream = stencilMD->stream_num_to_dep_proc_pairs[curr_dt_idx][dep][stream_num];
             int nrecv = zoid_pairs_at_stream.size() + send_dep_proc_pairs_at_stream.size();
             if (stream_num < NUM_SEND_STREAMS || nrecv > 0) {
                 dep_to_active_streams[dep].push_back(stream_num);
