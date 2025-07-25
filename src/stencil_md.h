47debafb6cef5967c012c244c5a64ce315d6e034
ryandeng1 <ryandeng@mit.edu>
2025-07-24 21:29:11 -0700

    progress sends more

--- src/stencil_md.h
+++ src/stencil_md.h
@@ -9387,6 +9387,13 @@ public:
                         src_stream_idx, dst_stream_idx, &r[send_request_idx]);
                     manager->m[src_stream_idx].unlock();
                     assert(res == MPI_SUCCESS);
+
+                    constexpr int num_iter = 10;
+                    for (int i = 0; i < num_iter; i++) {
+                        manager->m[src_stream_idx].lock();
+                        MPIX_Stream_progress(manager->streams[src_stream_idx]);
+                        manager->m[src_stream_idx].unlock();
+                    }
                 } else {
                     MPI_Isend(buf, zoid_ndoubles_send, MPI_DOUBLE, send_zoid_num % comm->nprocs, mpi_tag, 
                         all_comms[dst_stream_idx], &r[send_request_idx]);
