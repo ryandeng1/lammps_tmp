54f8f0186f4fde2c1a5badea9ecc73137a8029b5
ryandeng1 <ryandeng@mit.edu>
2025-08-03 18:47:14 -0700

    use better work queue

--- src/verlet.cpp
+++ src/verlet.cpp
@@ -3645,7 +3645,7 @@ void Verlet::run_stencil_md_many_cuts_proc_to_proc(int starting_timestep, double
         }
     }
 
-    constexpr bool USE_BETTER_WORK_QUEUE = false;
+    constexpr bool USE_BETTER_WORK_QUEUE = true;
     if (USE_BETTER_WORK_QUEUE) {
         std::vector<std::atomic<bool>> zoid_done(stencilMD->NUM_ZOIDS_MANY_CUTS);
         for (int dep = 0; dep < NUM_DEPS; dep++) {
@@ -3769,7 +3769,8 @@ void Verlet::run_stencil_md_many_cuts_proc_to_proc(int starting_timestep, double
             cilk_for (int stream_num = 0; stream_num < NUM_STREAMS; stream_num++) {
                 auto& zoid_pairs_at_stream = stencilMD->stream_num_to_zoid_pairs[curr_dt_idx][dep + 1][stream_num];
                 auto& send_dep_proc_pairs_at_stream = stencilMD->stream_num_to_dep_proc_pairs[curr_dt_idx][dep + 1][stream_num];
-                if (zoid_pairs_at_stream.size() + send_dep_proc_pairs_at_stream.size() > 0) {
+                int total_num_wait = zoid_pairs_at_stream.size() + send_dep_proc_pairs_at_stream.size();
+                if (total_num_wait > 0) {
                     stencilMD->RECEIVE_DATA_PROC_TO_PROC_AND_ZOID_TO_ZOID_STREAMS<curr_dt>(dep + 1, stream_num, DEFAULT_PIPELINE_STAGE, 
                         recv_r_zoid_to_zoid_streams[dep + 1][stream_num], stream_manager);
                 }
