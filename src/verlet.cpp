503ead21fa725f612be969bb2f0e9a711af317d1
ryandeng1 <ryandeng@mit.edu>
2025-08-04 18:38:55 -0700

    make progress thread lambda a function

--- src/verlet.cpp
+++ src/verlet.cpp
@@ -3567,6 +3567,20 @@ void Verlet::run_stencil_md_many_cuts_process_stream_better_work_queue(int start
     }
 }
 
+void start_progress_thread(MPIX_Stream_Manager* manager, std::atomic<bool>& done) noexcept {
+    while (!done.load(std::memory_order_acquire)) {
+        if (manager->global_lock.try_lock()) {
+            for (int stream_num = 0; stream_num < NUM_STREAMS; stream_num++) {
+                if (manager->m[stream_num].try_lock()) {
+                    MPIX_Stream_progress(manager->streams[stream_num]);
+                    manager->m[stream_num].unlock();
+                }
+            }
+            manager->global_lock.unlock();
+        }
+    }
+}
+
 template <bool curr_dt>
 void Verlet::run_stencil_md_many_cuts_proc_to_proc(int starting_timestep, double **test_f, double **test_x, double **test_v,
                                                     std::vector<std::atomic<int>>& zoid_recv_neighbor_counters,
@@ -3644,7 +3658,8 @@ void Verlet::run_stencil_md_many_cuts_proc_to_proc(int starting_timestep, double
             }
         };
 
-        cilk_spawn f(stream_manager, progress_thread_done);
+        // cilk_spawn f(stream_manager, progress_thread_done);
+        cilk_spawn start_progress_thread(stream_manager, progress_thread_done);
 
         /*
         cilk_spawn [this](MPIX_Stream_Manager* manager, std::atomic<bool>& done) noexcept {
