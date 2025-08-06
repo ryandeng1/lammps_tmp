1aeef029f9bcd4df63c0cff91cb147b3ca68d806
ryandeng1 <ryandeng@mit.edu>
2025-08-05 17:24:00 -0700

    more zoids + 16 streams

--- src/stencil_md.h
+++ src/stencil_md.h
@@ -53,7 +53,7 @@
 
 constexpr bool USE_BREAK = false;
 constexpr bool USE_STREAMS = true;
-constexpr int NUM_STREAMS = 8;
+constexpr int NUM_STREAMS = 16;
 constexpr int NUM_PROGRESS_STREAM_ITER = 25;
 
 // MinCostFlow class implementing a simple min-cost max-flow using SPFA.
@@ -970,8 +970,8 @@ public:
     std::vector<queue_info> my_queues_many_cuts[NUM_DEPS];
     std::vector<queue_info> my_queues_many_cuts_next_dt[NUM_DEPS];
 
-    static constexpr int NUM_CUTS_X = 4;
-    static constexpr int NUM_CUTS_Y = 4;
+    static constexpr int NUM_CUTS_X = 6;
+    static constexpr int NUM_CUTS_Y = 6;
     static constexpr int NUM_CUTS_Z = 4;
 
     static constexpr int NUM_ZOIDS_X = NUM_CUTS_X * 2;
