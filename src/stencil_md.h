28538501e799b69a628a849a0d8be934bbfb6773
ryandeng1 <ryandeng@mit.edu>
2025-08-05 22:12:01 -0700

    progress zoid to zoid stream

--- src/stencil_md.h
+++ src/stencil_md.h
@@ -54,7 +54,7 @@
 constexpr bool USE_BREAK = false;
 constexpr bool USE_STREAMS = true;
 constexpr int NUM_STREAMS = 24;
-constexpr int NUM_PROGRESS_STREAM_ITER = 0;
+constexpr int NUM_PROGRESS_STREAM_ITER = 10;
 
 // MinCostFlow class implementing a simple min-cost max-flow using SPFA.
 struct MinCostFlow {
@@ -3152,6 +3152,8 @@ public:
                         }
                     }
                 }
+
+                std::cout << "dep: " << dep << " zoid: " << zoid.num << " num diff proc: " << num_diff_proc << std::endl;
             }
 
             std::cout << "total num diff proc: " << total_num_diff_proc << std::endl;
