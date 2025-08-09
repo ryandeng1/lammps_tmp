f5b3d5bd9f7fb812864332cdaf8a7ba39bff98ec
ryandeng1 <ryandeng@mit.edu>
2025-08-09 14:03:26 -0700

    SW StencilMD

--- src/stencil_md_utils.h
+++ src/stencil_md_utils.h
@@ -48,24 +48,24 @@ constexpr int SW = 3;
 constexpr int TERSOFF = 4;
 constexpr int EAM = 5;
 
-constexpr int EXPERIMENT = DPD;
+constexpr int EXPERIMENT = SW;
 
 constexpr int NUM_DEPS = 4;
 
 constexpr int NUM_ZOIDS = 4 * 4 * 4;
 
-constexpr int NUM_TIMESTEPS_IN_PARALLEL = 6;
+constexpr int NUM_TIMESTEPS_IN_PARALLEL = 4;
 // constexpr double ADDITIONAL_CUTOFF = 0.4001;
 // constexpr double ADDITIONAL_CUTOFF = 0.4 + 1e-10;
 // constexpr double ADDITIONAL_CUTOFF = 1.0 + 1e-10;
-constexpr double ADDITIONAL_CUTOFF = 0.5 + 1e-10;
+constexpr double ADDITIONAL_CUTOFF = 2.0 + 1e-10;
 
 // this is for the potential, used when there is a multi-body potential
 constexpr double CUTOFF = 3.77;
-// constexpr double ALLEGRO_CUTOFF_RADIUS = 2 * CUTOFF;
+constexpr double ALLEGRO_CUTOFF_RADIUS = 2 * CUTOFF;
 // constexpr double ALLEGRO_CUTOFF_RADIUS = 1.12;
 // constexpr double ALLEGRO_CUTOFF_RADIUS = 2.5;
-constexpr double ALLEGRO_CUTOFF_RADIUS = 1.0;
+// constexpr double ALLEGRO_CUTOFF_RADIUS = 1.0;
 
 constexpr double MIDDLE_ZOID_WIDTH_RATIO = 0.5;
 
@@ -79,9 +79,9 @@ constexpr double ALLEGRO_SLOPE = ALLEGRO_CUTOFF_RADIUS + ADDITIONAL_CUTOFF;
 
 constexpr int NUM_WORKERS_PER_THREAD = 512;
 
-constexpr bool ONLY_RUN_LAMMPS = true;
+constexpr bool ONLY_RUN_LAMMPS = false;
 
-constexpr bool ONLY_RUN_STENCIL_MD = false;
+constexpr bool ONLY_RUN_STENCIL_MD = true;
 
 constexpr bool USE_BOND = true;
 
