f88527781bf4c18ad5440e25841034b36a8fea85
ryandeng1 <ryandeng@mit.edu>
2025-08-09 21:47:06 -0700

    grainsize 256

--- src/stencil_md.h
+++ src/stencil_md.h
@@ -13697,7 +13697,7 @@ public:
         const auto& local_idxs = zoid.local_idxs_per_timestep[timestep];
         const int nlocal = local_idxs.size();
 
-        constexpr int GRAINSIZE = 128;
+        constexpr int GRAINSIZE = 256;
 
         if (nlocal > GRAINSIZE) {
             #pragma cilk grainsize GRAINSIZE
