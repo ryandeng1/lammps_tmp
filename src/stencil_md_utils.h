17d183fdf88bcf26aa851fff8d503413d8ccaa6c
ryandeng1 <ryandeng@mit.edu>
2025-08-26 10:42:13 -0400

    try 8 * 8 * 4 zoids for 16 nodes

--- src/stencil_md_utils.h
+++ src/stencil_md_utils.h
@@ -54,7 +54,7 @@ constexpr int NUM_DEPS = 4;
 
 constexpr int NUM_ZOIDS = 4 * 4 * 4;
 
-constexpr int NUM_TIMESTEPS_IN_PARALLEL = 6;
+constexpr int NUM_TIMESTEPS_IN_PARALLEL = 4;
 // constexpr double ADDITIONAL_CUTOFF = 0.4001;
 constexpr double ADDITIONAL_CUTOFF = 0.4 + 1e-10;
 // constexpr double ADDITIONAL_CUTOFF = 1.0 + 1e-10;
