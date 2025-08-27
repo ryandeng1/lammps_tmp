ab2d4cc8ac949a97a658dde4e926c0d5284325ca
ryandeng1 <ryandeng@mit.edu>
2025-08-27 19:10:19 -0400

    huge lj memory = false

--- src/stencil_md.h
+++ src/stencil_md.h
@@ -13768,10 +13768,10 @@ public:
         constexpr int GRAINSIZE = 1024;
         constexpr int SMALL_GRAINSIZE = 128;
 
-        constexpr bool USE_MEMORY = true;
+        constexpr bool USE_MEMORY = false;
+        constexpr int LJ_GRAINSIZE = 512;
 
         if (USE_MEMORY) {
-            constexpr int LJ_GRAINSIZE = 512;
             if (nlocal <= LJ_GRAINSIZE) {
                 for (int idx = 0; idx < nlocal; idx++) {
                     int i = local_idxs[idx];
@@ -13948,70 +13948,8 @@ public:
             return;
         }
 
-        if ((dep == 0 && (timestep >  2)) || (dep == 3 && (timestep <= 2))) {
-            #pragma cilk grainsize SMALL_GRAINSIZE
-            cilk_for (int idx = 0; idx < nlocal; idx++) {
-                int i = local_idxs[idx];
-
-                const int itype = atom_type[i];
-
-                const auto &jlist = neighbor_list[i];
-                const double *_noalias const cutsqi = cutsq[itype];
-                const double *_noalias const offseti = offset[itype];
-                const double *_noalias const lj1i = lj1[itype];
-                const double *_noalias const lj2i = lj2[itype];
-                // const double *_noalias const lj3i = lj3[itype];
-                // const double *_noalias const lj4i = lj4[itype];
-
-                double xtmp = x[i].x;
-                double ytmp = x[i].y;
-                double ztmp = x[i].z;
-                int jnum = jlist.size();
-
-                double fxtmp = 0.0;
-                double fytmp = 0.0;
-                double fztmp = 0.0;
-
-                for (int jj = 0; jj < jnum; jj++) {
-                    double evdwl = 0.0;
-                    int j = jlist[jj];
-                    double factor_lj = special_lj[pair->sbmask(j)];
-                    j &= NEIGHMASK;
-
-                    double delx = xtmp - x[j].x;
-                    double dely = ytmp - x[j].y;
-                    double delz = ztmp - x[j].z;
-                    double rsq = delx * delx + dely * dely + delz * delz;
-                    int jtype = atom_type[j];
-
-                    if (rsq < cutsqi[jtype]) {
-                        double r2inv = 1.0 / rsq;
-                        double r6inv = r2inv * r2inv * r2inv;
-                        double forcelj = r6inv * (lj1i[jtype] * r6inv - lj2i[jtype]);
-                        double fpair = factor_lj * forcelj * r2inv;
-
-                        fxtmp += delx * fpair;
-                        fytmp += dely * fpair;
-                        fztmp += delz * fpair;
-
-                        if (newton_pair || j < nlocal) {
-                            spinlocks[j].lock();
-                            f[j].x -= delx * fpair;
-                            f[j].y -= dely * fpair;
-                            f[j].z -= delz * fpair;
-                            spinlocks[j].unlock();
-                        }
-                    }
-                }
-
-                spinlocks[i].lock();
-                f[i].x += fxtmp;
-                f[i].y += fytmp;
-                f[i].z += fztmp;
-                spinlocks[i].unlock();
-            }
-        } else if (nlocal > GRAINSIZE) {
-            #pragma cilk grainsize GRAINSIZE 
+        if (nlocal > LJ_GRAINSIZE) {
+            #pragma cilk grainsize LJ_GRAINSIZE
             cilk_for (int idx = 0; idx < nlocal; idx++) {
                 int i = local_idxs[idx];
 
