79e6a24da890f7e235f5cd21657ab7317fd0fb07
ryandeng1 <ryandeng@mit.edu>
2025-08-27 19:31:00 -0400

    stencilmd huge lj, 4k zoids, 512 grainsize

--- src/stencil_md.h
+++ src/stencil_md.h
@@ -970,9 +970,9 @@ public:
     std::vector<queue_info> my_queues_many_cuts[NUM_DEPS];
     std::vector<queue_info> my_queues_many_cuts_next_dt[NUM_DEPS];
 
-    static constexpr int NUM_CUTS_X = 6;
-    static constexpr int NUM_CUTS_Y = 6;
-    static constexpr int NUM_CUTS_Z = 6;
+    static constexpr int NUM_CUTS_X = 4;
+    static constexpr int NUM_CUTS_Y = 4;
+    static constexpr int NUM_CUTS_Z = 4;
 
     static constexpr int NUM_ZOIDS_X = NUM_CUTS_X * 2;
     static constexpr int NUM_ZOIDS_Y = NUM_CUTS_Y * 2;
@@ -13769,7 +13769,7 @@ public:
         constexpr int SMALL_GRAINSIZE = 128;
 
         constexpr bool USE_MEMORY = true;
-        constexpr int LJ_GRAINSIZE = 1024;
+        constexpr int LJ_GRAINSIZE = 512;
 
         if (USE_MEMORY) {
             if (nlocal <= LJ_GRAINSIZE) {
