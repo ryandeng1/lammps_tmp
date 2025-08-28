7f73a1d81a3ab871c17a7f24406a2edb66ce4dfc
ryandeng1 <ryandeng@mit.edu>
2025-08-28 14:36:53 -0400

    stencilmd dpd more zoids

--- src/stencil_md_utils.h
+++ src/stencil_md_utils.h
@@ -54,7 +54,7 @@ constexpr int NUM_DEPS = 4;
 
 constexpr int NUM_ZOIDS = 4 * 4 * 4;
 
-constexpr int NUM_TIMESTEPS_IN_PARALLEL = 8;
+constexpr int NUM_TIMESTEPS_IN_PARALLEL = 4;
 constexpr double ADDITIONAL_CUTOFF = 0.5 + 1e-10;
 
 // this is for the potential, used when there is a multi-body potential
