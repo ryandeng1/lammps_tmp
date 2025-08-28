b7bc0364b4b5e24ebd3cef5806e571e5b710ccaf
ryandeng1 <ryandeng@mit.edu>
2025-08-28 16:57:36 -0400

    12 nodes, back to original implementation??

--- src/stencil_md.h
+++ src/stencil_md.h
@@ -53,8 +53,8 @@
 
 constexpr bool USE_BREAK = false;
 constexpr bool USE_STREAMS = true;
-constexpr int NUM_STREAMS = 20;
-constexpr int NUM_PROGRESS_STREAM_ITER = 25;
+constexpr int NUM_STREAMS = 24;
+constexpr int NUM_PROGRESS_STREAM_ITER = 10;
 
 // MinCostFlow class implementing a simple min-cost max-flow using SPFA.
 struct MinCostFlow {
