8ab170fcd7499a571eb8a2bc6a24a5d86012de56
ryandeng1 <ryandeng@mit.edu>
2025-08-28 16:12:27 -0400

    try another approach

--- src/verlet.cpp
+++ src/verlet.cpp
@@ -3633,8 +3633,7 @@ void Verlet::run_stencil_md_many_cuts_proc_to_proc(int starting_timestep, double
 
     std::vector<std::atomic_flag> zoid_unpack_claimed(stencilMD->NUM_ZOIDS_MANY_CUTS);
 
-    constexpr bool USE_BETTER_WORK_QUEUE = true;
-    constexpr bool ALL_DEPS = false;
+    constexpr bool USE_BETTER_WORK_QUEUE = false;
     if (USE_BETTER_WORK_QUEUE) {
         cilk_scope {
             std::vector<std::atomic<bool>> zoid_done(stencilMD->NUM_ZOIDS_MANY_CUTS);
