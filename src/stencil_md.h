6e4d66807a9e67223b6b165331c0e019fbea2d6c
ryandeng1 <ryandeng@mit.edu>
2025-11-03 23:00:46 -0500

    dpd 2k zoids

--- src/stencil_md.h
+++ src/stencil_md.h
@@ -982,8 +982,8 @@ public:
     std::vector<queue_info> my_queues_many_cuts[NUM_DEPS];
     std::vector<queue_info> my_queues_many_cuts_next_dt[NUM_DEPS];
 
-    static constexpr int NUM_CUTS_X = 6;
-    static constexpr int NUM_CUTS_Y = 4;
+    static constexpr int NUM_CUTS_X = 8;
+    static constexpr int NUM_CUTS_Y = 8;
     static constexpr int NUM_CUTS_Z = 4;
 
     static constexpr int NUM_ZOIDS_X = NUM_CUTS_X * 2;
