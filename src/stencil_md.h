c8d528c8e556bb19da231448a1016b88f1d88278
ryandeng1 <ryandeng@mit.edu>
2026-02-12 21:29:56 -0500

    dpd dt=2

--- src/stencil_md.h
+++ src/stencil_md.h
@@ -55,7 +55,7 @@
 
 constexpr bool USE_BREAK = false;
 constexpr bool USE_STREAMS = true;
-constexpr int NUM_STREAMS = 24;
+constexpr int NUM_STREAMS = 48;
 constexpr int NUM_PROGRESS_STREAM_ITER = 10;
 
 // inline double s_compute_time[24] = {0};
@@ -1067,7 +1067,7 @@ public:
 
     static constexpr int NUM_CUTS_X = 8;
     static constexpr int NUM_CUTS_Y = 8;
-    static constexpr int NUM_CUTS_Z = 4;
+    static constexpr int NUM_CUTS_Z = 8;
 
     static constexpr int NUM_ZOIDS_X = NUM_CUTS_X * 2;
     static constexpr int NUM_ZOIDS_Y = NUM_CUTS_Y * 2;
