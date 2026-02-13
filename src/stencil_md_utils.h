24623df037c72cb34ed781cc319b2a849d3e3a6b
ryandeng1 <ryandeng@mit.edu>
2026-02-12 21:12:27 -0500

    dpd dt=6

--- src/stencil_md_utils.h
+++ src/stencil_md_utils.h
@@ -49,8 +49,8 @@ constexpr int TERSOFF = 4;
 constexpr int EAM = 5;
 
 // constexpr int EXPERIMENT = BOND_FENE;
-constexpr int EXPERIMENT = LJ;
-// constexpr int EXPERIMENT = DPD;
+// constexpr int EXPERIMENT = LJ;
+constexpr int EXPERIMENT = DPD;
 // constexpr int EXPERIMENT = SW;
 // constexpr int EXPERIMENT = TERSOFF;
 
@@ -58,10 +58,10 @@ constexpr int NUM_DEPS = 4;
 
 constexpr int NUM_ZOIDS = 4 * 4 * 4;
 
-constexpr int NUM_TIMESTEPS_IN_PARALLEL = 2;
+constexpr int NUM_TIMESTEPS_IN_PARALLEL = 6;
 // constexpr double ADDITIONAL_CUTOFF = 0.4 + 1e-10;
-constexpr double ADDITIONAL_CUTOFF = 0.3 + 1e-10;
-// constexpr double ADDITIONAL_CUTOFF = 0.5 + 1e-10;
+// constexpr double ADDITIONAL_CUTOFF = 0.3 + 1e-10;
+constexpr double ADDITIONAL_CUTOFF = 0.5 + 1e-10;
 // constexpr double ADDITIONAL_CUTOFF = 2.0 + 1e-10;
 
 // this is for the potential, used when there is a multi-body potential
@@ -69,8 +69,8 @@ constexpr double ADDITIONAL_CUTOFF = 0.3 + 1e-10;
 constexpr double CUTOFF = 3.2;
 // constexpr double ALLEGRO_CUTOFF_RADIUS = 2 * CUTOFF;
 // constexpr double ALLEGRO_CUTOFF_RADIUS = 1.12;
- constexpr double ALLEGRO_CUTOFF_RADIUS = 2.5;
-// constexpr double ALLEGRO_CUTOFF_RADIUS = 1.0;
+//  constexpr double ALLEGRO_CUTOFF_RADIUS = 2.5;
+constexpr double ALLEGRO_CUTOFF_RADIUS = 1.0;
 
 constexpr bool DEBUG_SEND_RECV_DATA = false;
 
